45ee2530a76d463543b3b8dee4ad0044554e2b3d
Michael Short <mshort0403@gmail.com>
2024-01-30 12:30:39 -0800

    Add capstone

--- src/components/Sorry.js
+++ src/components/Sorry.js
@@ -3,7 +3,7 @@ import '../styles/Sorry.css';
 function Sorry() {
 	return (
 		<div id='sorry'>
-			<h1>Oops! This page has not been implemented for this project.</h1>
+			<h1>Sorry! This page has not been implemented for this project.</h1>
 		</div>
 	);
 }
